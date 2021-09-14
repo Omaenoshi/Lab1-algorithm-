ba979ec5ce4f0dd915d7943398b9692bb9822e2a
Omaenoshi <71465546+Omaenoshi@users.noreply.github.com>
2021-09-14 14:46:36 +0500

    aed

--- lab1beta/lab1beta/Program.cs
+++ lab1beta/lab1beta/Program.cs
@@ -8,58 +8,93 @@ namespace lab1beta
 {
     class Program
     {
+        static int[] vector = new int[2000];
+
+        delegate double CurrentFunc(int i);
+
         static void Main(string[] args)
         {
+            CreateVector();
+
+            Start(FourthFunc);
+         
+        }
+
+        static void Start(CurrentFunc currentFunc)
+        {
             string[] results = new string[2000];
-            for (int i = 1; i < 2000; i++)
+        
+            for (int i = 0; i < 2000; i++)
             {
                 Stopwatch stopwatch = new Stopwatch();
                 stopwatch.Start();
-                ThirdFunc(GenerateFunc(i));
+                currentFunc(i);
                 stopwatch.Stop();
                 TimeSpan ts = stopwatch.Elapsed;
                 decimal res = (decimal)ts.TotalMilliseconds;
                 Console.WriteLine(res);
-                results[i] = $"{i} : {res}";
+                results[i] = $"{i}.{res}";
             }
 
             File.WriteAllLines(@"../../../TextFile1.csv", results);
         }
 
-        static int FirstFunc(int n)
+        static void CreateVector()
+        {
+            Random rnd = new Random();
+            for (var i = 0; i < 2000; i++)
+                vector[i] = rnd.Next();
+        }
+
+        static double FirstFunc(int n)
         {
             return 1;
         }
 
-        static int[] GenerateFunc(int n)
-        {
-            int[] arr = new int[n];
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Random rnd = new Random();
-                arr[i] = rnd.Next();
-            }
-            return arr;
-        }
-
-        static int SecondFunc(int[] arr)
+        static double SecondFunc(int i)
         {
             int result = 0;
-            for(var i = 0; i < arr.Length; i++)
+            for(var j = 0; j < i; j++)
             {
-                result += arr[i];
+                result += vector[j];
             }
             return result;
         }
 
-        static int ThirdFunc(int[] arr)
+        static double ThirdFunc(int i)
         {
             int result = 0;
-            for (var i = 0; i < arr.Length; i++)
+            for (var j = 0; j < i; j++)
             {
-                result *= arr[i];
+                result *= vector[j];
             }
             return result;
+        }
+
+        static double FourthFunc(int i)
+        {
+            double x = 1.5;
+            var result = 0.0;
+            for (var j = 0; j < i; j++)
+            {
+                result += vector[i] * Math.Pow(x, i);
+            }
+            return result;
+        }
+
+        static double BubbleSort(int i)
+        {
+            for (var j = 0; j < i - 1; j++)
+            {
+                if (vector[j] < vector[j+1])
+                {
+                    int num = vector[j];
+                    vector[j] = vector[j + 1];
+                    vector[j + 1] = num;
+                }
+            }
+
+            return 0;
         }
     }
 }
